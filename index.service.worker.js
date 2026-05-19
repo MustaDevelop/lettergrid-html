22b1ca86827bfa01a9d4505081996d7c4191f2d7
MustaDevelop <mustadevelop@proton.me>
2026-05-19 21:11:27 +0300

    Mobile Buttons Fix 2

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779213943|1538709871';
+const CACHE_VERSION = '1779214278|1872852048';
 /** @type {string} */
 const CACHE_PREFIX = 'Letter Grid-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
